6f08a4ae931c3697580c257524cdcd54f74063c4
KyeongminKim <rlarudals016@naver.com>
2018-07-17 11:42:33 +0900

    test

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -1,15 +1,10 @@
-//
-//  main.cpp
-//  BinarySearch
-//
-//  Created by MacUser on 2018. 7. 17..
-//  Copyright © 2018년 MacUser. All rights reserved.
-//
+#include <cstdio>
+#include <algorithm>
+using namespace std;
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    puts("0");
+    for(int i=0;i<5;i++){
+        puts("");
+    }
 }
