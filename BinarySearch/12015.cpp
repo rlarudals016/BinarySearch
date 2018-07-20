8327ff68651e5f18a20209926f773bc06ffa0001
KyeongminKim <rlarudals016@naver.com>
2018-07-20 11:22:44 +0900

    이 문제는 어떠한 수열에서 가장 긴 증가하는 부분수열을 구하는 문제이다. 수열의 크기가 100만이기 때문에 O(n^2)에는 풀 수 없는 문제이다. 따라서 nlogn으로 풀 수 있는 이진탐색을 이용하여야 한다. 원래의 수열을 담을 벡터 vt와, 해당 인덱스 i 길이만큼 만들 수 있는 부분 수열중 가장 작은 값을 담는 벡터를 또 하나 만든다. 이렇게 벡터 두개를 만들게 되면 원래의 벡터 n만큼 반복문을 돌면서 lower_bound 함수를 이용하여 두번째 벡터를 만들면 된다. 최종적으로 두번째 벡터의 사이즈가 답이 된다.

---
+++ BinarySearch/12015.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> vt,ans;
+
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int num;
+        scanf("%d",&num);
+        vt.push_back(num);
+    }
+    ans.push_back(vt[0]);
+    int size=0;
+    for(int i=1;i<n;i++){
+        if(ans[size]<vt[i]){
+            size++;
+            ans.push_back(vt[i]);
+            continue;
+        }
+        int idx=lower_bound(ans.begin(),ans.end(),vt[i])-ans.begin();
+        ans[idx]=vt[i];
+    }
+    printf("%d",size+1);
+}
